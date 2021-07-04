246b116b0fcd53232bb1027eec48b1591b7209d0
vinnie-grapes <vinnie-grapes@yandex.ru>
2021-07-04 16:13:27 +0300

    add first tests

---
+++ src/test/java/com/tradesanta/tests/TestBase.java
@@ -0,0 +1,41 @@
+package com.tradesanta.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.tradesanta.config.Project;
+import com.tradesanta.helpers.AllureAttachments;
+import com.tradesanta.helpers.DriverSettings;
+import com.tradesanta.helpers.DriverUtils;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.junit5.AllureJunit5;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+
+@ExtendWith({AllureJunit5.class})
+public class TestBase {
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        DriverSettings.configure();
+        Configuration.startMaximized = true;
+    }
+
+    @AfterEach
+    public void addAttachments() {
+        String sessionId = DriverUtils.getSessionId();
+
+        AllureAttachments.addScreenshotAs("Last screenshot");
+        AllureAttachments.addPageSource();
+//        AllureAttachments.attachNetwork(); // todo
+        AllureAttachments.addBrowserConsoleLogs();
+
+        Selenide.closeWebDriver();
+
+        if (Project.isVideoOn()) {
+            AllureAttachments.addVideo(sessionId);
+        }
+    }
+}
